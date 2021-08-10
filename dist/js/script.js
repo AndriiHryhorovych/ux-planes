3faa165c966a66ec9cd7ca764102396c5f05b75a
AndriiHryhorovych <59657519+AndriiHryhorovych@users.noreply.github.com>
2021-08-10 18:55:03 +0300

    finfshed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,29 @@
 
+// Спрацьовує кожного разу при прокручуванні сторінки
+$(window).on("scroll", function(){
+
+	// Отримуємо, на скільки пікселів прокручена сторінка
+	var distance = $(window).scrollTop();
+
+	// Додаємо або видаляємо клас в залежності від прокрученої сторінки
+	if (distance >= 50) {
+		$('.header').addClass('scrolled')
+	} else {
+		$('.header').removeClass('scrolled')
+	}
+})
+
+
+
+$(document).ready(function(){
+	window.setTimeout(function(){
+		$(".plane").addClass("animated")
+	},500)
+})
+
+$(document).on('click','.header__menu-toggle',function(){
+	$('.header__menu').toggleClass('opened');
+	$('.header').toggleClass('menu-opened');
+})
 
 
